2e07da4defa42af00a7600bc9a41ba278ddd0c89
Robin Crifo <robincrifo@Ordinateur-portable-de-Robin.local>
2024-11-16 21:52:31 +0100

    404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AcceuilPage from "./Page/AcceuilPage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import "./assets/style.css"
 import "./assets/layout.css"
 import "./assets/parcours.css"
@@ -9,18 +9,36 @@ import AproposPage from './Page/AproposPage';
 import CVPage from "./Page/CVPage";
 import { ImageProvider } from './context/ImageContext';
 
+function RedirectHandler() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const redirect = params.get("redirect");
+
+        if (redirect) {
+            navigate(redirect, { replace: true });
+        }
+    }, [location, navigate]);
+
+    return null;
+}
+
 function App() {
     return (
-        <Router basename="/Portfolio2">
+        <BrowserRouter basename="/Portfolio2">
+            <RedirectHandler />
             <Routes>
                 <Route path="/" element={<AcceuilPage />} />
                 <Route path="/parcours" element={
                     <ImageProvider>
-                        <AproposPage />
-                    </ImageProvider>} />
-                <Route path="/cv" element={<CVPage/>} />
+                        <AproposPage/>
+                    </ImageProvider>
+                    }   />
+                <Route path="/cv" element={<CVPage />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
